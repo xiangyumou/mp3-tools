d4ec39abe01bf47ca4a15483e54b924bf29b205e
Xiangyu <xiangyu.moe@gmail.com>
2026-01-06 16:54:06 +0800

    build: Remove Electron application, its dependencies, build scripts, and release workflow.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
   // Images unoptimization is required for static export unless using a loader
   images: {
     unoptimized: true
